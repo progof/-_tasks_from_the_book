0e1e7b78f4b01c57a5cea12e80dec22934a5f61c
progof <34421475+progof@users.noreply.github.com>
2023-02-20 19:26:04 +0100

    Replaced the operator if not with a switch

--- 1_cards/cards.c
+++ 1_cards/cards.c
@@ -6,10 +6,10 @@
 int main()
 {
     char card_name[3];
-    puts("Enter the name of the card: ");
+    puts("Enter the name of the card:");
     scanf("%2s", card_name);
     short val = 0;
-
+/*
     if(card_name[0] == 'K'){
         val = 10;
     }else if(card_name[0] == 'Q'){
@@ -20,6 +20,24 @@ int main()
         val = 11;
     }else{
         val = atoi(card_name);
+    }
+*/
+
+    switch (card_name[0])
+    {
+    case 'K':
+    case 'Q':
+    case 'J':
+        val = 10;
+        break;
+
+    case 'A':
+        val = 11;
+        break;        
+
+    default:
+        val = atoi(card_name);
+        break;
     }
 
     if ((val > 2) && (val < 7))
