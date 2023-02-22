c18bbf6ec9eac0edd635000a0ad13759e5f9dc0c
progof <34421475+progof@users.noreply.github.com>
2023-02-22 16:48:25 +0100

    The value of the variables was passed to the function via pointers

--- 2_memory_and_pointers/main.c
+++ 2_memory_and_pointers/main.c
@@ -1,10 +1,10 @@
 // Tracking a player by longitude and latitude
 #include <stdio.h>
 
-void go_south_east(short lat, short lon)
+void go_south_east(short *lat, short *lon)
 {
-    lat = lat - 1;
-    lon = lon + 1;
+    *lat = *lat - 1;
+    *lon = *lon + 1;
 }
 
 int main()
@@ -12,7 +12,7 @@ int main()
     short lantitude = 32;
     short longitude = -64;
 
-    go_south_east(lantitude, longitude);
+    go_south_east(&lantitude, &longitude);
 
     printf("Stop! Now our coordinates: [%i, %i]\n", lantitude, longitude);
 
